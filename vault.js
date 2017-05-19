9587f30ce797a95c4fb76e448f6e656a55c4d6aa
clarkdenunzio <clark@clarkdenunzio.com>
2017-05-18 19:50:51 -1000

    asdgasd

--- vault.js
+++ vault.js
@@ -1,4 +1,22 @@
 'use strict';
 module.exports = function() {
-  
+
+  var vault = (function(){
+
+    function setValue(key, value){
+      this.key = value;
+    };
+
+    function getValue(key){
+      return this.key;
+    };
+
+    return {
+      setValue: setValue,
+      getValue: getValue
+    };
+
+  })();
+
+
 };
